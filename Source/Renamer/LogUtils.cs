3e697fa8e78a7b727b765e2adaf92d7c82cf32ea
siimav <siim.aaver@gmail.com>
2021-09-13 15:04:24 +0300

    Make the LogUtils actually do something on debug builds

--- Source/Renamer/LogUtils.cs
+++ Source/Renamer/LogUtils.cs
@@ -1,14 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using UnityEngine;
 
 namespace Renamer
 {
-    class LogUtils
+    internal class LogUtils
     {
         private const string logName = "KerbalRenamer";
-        private static bool enabled = false;
+        internal static bool enabled = false;
+
         public static void Log(params object[] message)
         {
             Log(Array.ConvertAll(message, item => item.ToString()));
@@ -16,16 +15,16 @@ namespace Renamer
 
         public static void Log(params string[] message)
         {
-            if (!enabled) {
-                return;
-            }
+            #if !DEBUG
+            if (!enabled) return;
+            #endif
+
             var builder = StringBuilderCache.Acquire();
             builder.Append("[").Append(logName).Append("] ");
-            builder.Append(DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fff")).Append(" - ");
             foreach (string part in message) {
                 builder.Append(part);
             }
-            UnityEngine.Debug.Log(builder.ToStringAndRelease());
+            Debug.Log(builder.ToStringAndRelease());
         }
     }
 }
