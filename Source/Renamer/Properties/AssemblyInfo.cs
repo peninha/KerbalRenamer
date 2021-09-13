9e62640001284a35df9f5c62724c034c78ebd814
siimav <siim.aaver@gmail.com>
2021-09-13 16:38:42 +0300

    Prep for release

--- Source/Renamer/Properties/AssemblyInfo.cs
+++ Source/Renamer/Properties/AssemblyInfo.cs
@@ -8,15 +8,13 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("justin, KSP-RO")]
+[assembly: AssemblyCopyright ("Justin Bengtson, KSP-RO")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
+
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("1.4.0")]
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyVersion ("1.5.0")]
 
+[assembly: KSPAssembly("Renamer", 1, 5)]
